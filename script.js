a189eceb8e09253d9aa7c4117baeaf169c1669d4
Pråðëëp <im.pradeepx@gmail.com>
2024-03-05 10:28:11 +0530

    temp converter

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const farenhit = document.getElementById("3").value;
+
+
+function result()
+{ if{const celcios = document.getElementById("1").value;
+    const answer = celcios/2;}else if{  const kelvin = document.getElementById("2").value;
+    answer =kelvin/2;} else{ answer= farenhit/2;}
+document.getElementById("res").innerHTML=answer;
